76ff6533cecfdc76d2115ad7f1bed0bbedde3771
henry54decimus <31940683+henry54decimus@users.noreply.github.com>
2017-09-20 18:32:21 -0500

    Update Payroll.h
    Function prototypes of payroll program.

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,15 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
+double getRegularHours(double totalHours);
+double getOvertimeHours(double totalHours);
+double getGrossPay(double regularHours, double overtimeHours, double hourlyRate);
+double getGrossPay(double salary);
+double getFIT(double grossPay);
+double getFICASSN(double grossPay);
+double getFICAMED(double grossPay);
+double getNetPay(double grossPay, double fedincome, double ficaSSN, double ficaMedicare);
+void printCheck(double grossPay,  double regularHours, double overtimeHours, double fedincome, double ficaSSN, double ficaMedicare, double netPay);
 
 
 #endif
