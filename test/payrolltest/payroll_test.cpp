846513eec472947583538655dd19de75d6a8dec7
henry54decimus <31940683+henry54decimus@users.noreply.github.com>
2017-09-20 20:31:23 -0500

    update to make test for travis

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,10 +1,12 @@
 #include "gtest/gtest.h"
 #include "Payroll.h"
+#include <string>
+using namespace std;
 
 TEST(pay_rollTest, testing_FIT){
   EXPECT_EQ(6.75, getFIT(45));
 }
 
 TEST(pay_rollTest, testing_FICAMED){
-  EXPECT_EQ(1, getFICAMED(100));
+  EXPECT_EQ("1.45",to_string ( getFICAMED(100)));
 }
