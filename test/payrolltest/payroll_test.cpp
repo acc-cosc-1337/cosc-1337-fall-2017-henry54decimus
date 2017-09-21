6379f03def50d84f3f562c6628c5bd0c54e9b1f4
henry54decimus <31940683+henry54decimus@users.noreply.github.com>
2017-09-20 20:17:44 -0500

    2nd update to travis

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 #include "Payroll.h"
 
-Test(pay_rollTest, testing_FIT){
+TEST(pay_rollTest, testing_FIT){
   EXPECT_EQ(6.75, getFIT(45));
 }
 
-Test(pay_rollTest, testing_FICAMED){
+TEST(pay_rollTest, testing_FICAMED){
   EXPECT_EQ(.6525, getFICAMED(45));
 }
