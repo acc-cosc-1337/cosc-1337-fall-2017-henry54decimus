a540ad8601dac75229e3a6ab98ffda9229847c26
henry54decimus <31940683+henry54decimus@users.noreply.github.com>
2017-09-20 19:07:50 -0500

    created unit test for my functions

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,1 +1,10 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+Test(payrollTest, testing_FIT){
+  EXPECT_EQ(6.75, getFIT(45));
+}
+
+Test(payrollTest, testing_FICAMED){
+  EXPECT_EQ(.6525, getFICAMED(45));
+}
